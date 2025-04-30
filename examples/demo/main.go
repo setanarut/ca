14ce682ff31a9b66113433b1fed66970f1a5a2e7
Barış <duurkak@yandex.com>
2025-04-30 19:31:50 +0300

    ticker

--- examples/demo/main.go
+++ examples/demo/main.go
@@ -14,23 +14,44 @@ var S = ca.S
 
 func main() {
 
-	// otomat := ca.NewAutomaton(512, 256, 33, ca.DunesRule(32))
+	otomat := ca.NewAutomaton(1280, 720, 33, ca.DunesRule(32))
 	// otomat := ca.NewAutomaton(512, 256, 33, CyclicRule(1, 1, 24, Moore))
 
 	// otomat := ca.NewAutomaton(512, 256, 3, ca.LifeRule(B(3), S(2, 3)))
-	otomat := ca.NewAutomaton(512, 256, 3, ca.LifeRule(B(3), S(0, 1, 2, 3, 4, 5, 6, 7, 8)))
-	otomat.LifeLikeAgeEnabled = true
-	// otomat.FillWithRandomStates()
+	// otomat := ca.NewAutomaton(512, 256, 3, ca.LifeRule(B(3), S(0, 1, 2, 3, 4, 5, 6, 7, 8)))
+	// otomat.LifeLikeAgeEnabled = true
+	otomat.FillWithRandomStates()
 
-	ca.Birth(otomat.Current, 100-2, 100-3)
-	ca.Birth(otomat.Current, 100, 100)
-	ca.Birth(otomat.Current, 102, 103)
+	// ca.Birth(otomat.Current, 100-2, 100-3)
+	// ca.Birth(otomat.Current, 100, 100)
+	// ca.Birth(otomat.Current, 102, 103)
 
 	t := time.Now()
-	for range 700 {
+	frames := 0
+	done := make(chan bool)
+
+	// Saniyede bir güncelleme yapan goroutine
+	go func() {
+		ticker := time.NewTicker(time.Second)
+		defer ticker.Stop()
+
+		for {
+			select {
+			case <-ticker.C:
+				fmt.Printf("\rİşlenen kare sayısı: %d", frames)
+			case <-done:
+				return
+			}
+		}
+	}()
+
+	for range 4000 {
 		otomat.Step()
+		frames++
 	}
-	fmt.Println("Simulation time taken:", time.Since(t).Seconds(), "seconds")
+
+	done <- true
+	fmt.Println("\nSimulation time taken:", time.Since(t).Seconds(), "seconds")
 
 	err := imgio.Save("examples/demo/o.png", otomat.RenderedImage(), imgio.PNGEncoder())
 	if err != nil {
